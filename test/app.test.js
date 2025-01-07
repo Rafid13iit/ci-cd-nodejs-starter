9462f508561a9095ed06b7441a8feaa0af425457
Rafid13iit <rafidssrr.6767@gmail.com>
2025-01-07 08:47:23 +0600

    app.test.js has some changes

--- test/app.test.js
+++ test/app.test.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const app = require('../server');
 
 describe('GET /', () => {
-    it('responds with Hello, CI/CD with Vercel!', async () => {
+    it('responds with Hello, CI/CD with GitHub Actions and Vercel!', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toEqual(200);
-        expect(res.text).toEqual('Hello, CI/CD with Vercel! Let\'s test this!'); 
+        expect(res.text).toEqual('Hello, CI/CD with GitHub Actions and Vercel!');
     });
 });
