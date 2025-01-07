a697cdbc69b332c7ade87ba0324c6e451efbba30
Rafid13iit <rafidssrr.6767@gmail.com>
2025-01-07 11:01:45 +0600

    files changed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hello, CI/CD with GitHub Actions and Vercel! Good");
+    res.send("Hello, CI/CD with GitHub Actions and Vercel! Good, very good");
 });
 
 const port = process.env.PORT || 3000;
